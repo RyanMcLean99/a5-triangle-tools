6f55d50ef67b335b88b2020c1e5b1e7e7b5d9533
RyanMcLean99 <rrm00016@students.stir.ac.uk>
2023-10-31 16:35:39 +0000

    end of practical 4

--- Triangle.Compiler/src/main/java/triangle/codeGenerator/Encoder.java
+++ Triangle.Compiler/src/main/java/triangle/codeGenerator/Encoder.java
@@ -105,6 +105,7 @@ import triangle.abstractSyntaxTrees.vnames.SimpleVname;
 import triangle.abstractSyntaxTrees.vnames.SubscriptVname;
 import triangle.abstractSyntaxTrees.vnames.Vname;
 import triangle.codeGenerator.entities.AddressableEntity;
+import triangle.codeGenerator.entities.BarPrimitiveRoutine;
 import triangle.codeGenerator.entities.EqualityRoutine;
 import triangle.codeGenerator.entities.FetchableEntity;
 import triangle.codeGenerator.entities.Field;
@@ -188,12 +189,10 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 
 	@Override
 	public Void visitRepeatCommand(RepeatCommand ast, Frame frame) {
-		var jumpAddr = emitter.emit(OpCode.JUMP, 0, Register.CB, 0);
 		var loopAddr = emitter.getNextInstrAddr();
 		ast.C.visit(this, frame);
-		emitter.patch(jumpAddr);
 		ast.E.visit(this, frame);
-		emitter.emit(OpCode.JUMPIF, Machine.trueRep, Register.CB, loopAddr);
+		emitter.emit(OpCode.JUMPIF, Machine.falseRep, Register.CB, loopAddr);
 		return null;
 	}
 	// Expressions
@@ -749,6 +748,7 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		elaborateStdPrimRoutine(StdEnvironment.puteolDecl, Primitive.PUTEOL);
 		elaborateStdEqRoutine(StdEnvironment.equalDecl, Primitive.EQ);
 		elaborateStdEqRoutine(StdEnvironment.unequalDecl, Primitive.NE);
+		StdEnvironment.barDecl.entity = new BarPrimitiveRoutine();
 	}
 
 	boolean tableDetailsReqd;
